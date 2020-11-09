3d3b49c8d9d78e6e0fc73f3488ab3db4e9494625
Arkady Emelyanov <arkady.emelyanov@gmail.com>
2020-11-09 10:05:51 +0300

    batch -> table

--- client.py
+++ client.py
@@ -1,12 +1,10 @@
-import pandas as pd
-import pyarrow as pa
 import pyarrow.flight as fl
 
 
 def main():
     client = fl.FlightClient(location="grpc://127.0.0.1:9092")
-    batch_handler = client.do_get(fl.Ticket('cities')).read_all()
-    df = batch_handler.to_pandas()
+    table = client.do_get(fl.Ticket('cities')).read_all()
+    df = table.to_pandas()
     print(df)
 
 
